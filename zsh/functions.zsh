1933618e54ecb56388f6c472d99015471b625aaf
Joris Noordermeer <jo.noordermeer@gmail.com>
2018-10-06 21:36:47 +0200

    Update zsh settings

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -11,14 +11,14 @@ mkd () {
 }
 
 # Change working directory to the top-most Finder window location
-# cdf () {
-#     target=`osascript -e 'tell application "Finder" to if (count of Finder windows) > 0 then get POSIX path of (target of front Finder window as text)'`
-#     if [ "$target" != "" ]; then
-#         cd "$target"; pwd
-#     else
-#         echo 'No Finder window found' >&2
-#     fi
-# }
+ cdf () {
+     target=`osascript -e 'tell application "Finder" to if (count of Finder windows) > 0 then get POSIX path of (target of front Finder window as text)'`
+     if [ "$target" != "" ]; then
+         cd "$target"; pwd
+     else
+         echo 'No Finder window found' >&2
+     fi
+ }
 
 # To create a ZIP archive of a folder
 zipf () { zip -r "$1".zip "$1" ; }

2a757c466b5c9226275b52d32846dbc283d038f5
Joris Noordermeer <accounts@noordermeer.xyz>
2019-06-05 13:01:20 +0200

    Remove aliases already loaded via zsh plugin

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -18,9 +18,6 @@ alias hu='homestead up'
 alias he='(open ~/Homestead/Homestead.yaml)'
 alias vm='ssh vagrant@127.0.0.1 -p 2222'
 
-# Laravel
-function artisan() { php artisan "$@" }
-
 # NPM
 alias nw='npm run watch'
 alias np='npm run prod'
@@ -40,18 +37,11 @@ alias p='cd /Users/joris/Sites/projects'
 alias sshkey='(cat ~/.ssh/id_rsa.pub | pbcopy; echo "Copied SSH Key to clipboard.")'
 
 # GIT
-alias gs='git status'
-alias ga='git add'
-alias gaa='git add --all'
-alias gp='git push'
-function gc() { git commit -m "$@" }
-function gca() { git add --all; git commit -m "$@" }
-alias gd='git diff'
+alias gs='git status -s'
+alias gcm='git commit -m'
 alias go='git checkout'
 alias gom='git checkout master'
 alias god='git checkout develop'
-alias gl='git log --oneline --decorate --all --graph'
-alias grhh='git reset --hard'
 
 alias publish='(git checkout develop; git push; git push --tags; git checkout master; git push; git checkout develop)'
 alias pull='(git checkout develop; git pull; git pull --tags; git checkout master; git pull; git checkout develop)'

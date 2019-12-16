c42b418deba10617719254fe5a62fca2ab3c75ea
Joris Noordermeer <joris@noordermeer.ch>
2019-12-16 15:06:04 +0100

    Update the maintenance script

--- maintenance.sh
+++ maintenance.sh
@@ -1,27 +1,51 @@
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Store the current directory
 cwd=$(pwd)
 
+# Upgrade Brew packages
 brew update
 brew upgrade
 
 # Delete git completion in order for git flow completion to work
 rm -f /usr/local/share/zsh/site-functions/_git
 
+# Upgrade Applications
 brew cu -y
 mas upgrade
 
+# Cleanup Brew packages
 brew cleanup
 
+# Upgrade Oh-My-Zsh
 upgrade_oh_my_zsh
 
+# Update git repositories
 (cd ~/Homestead && git pull)
 (cd ~/Sites/internal/spark-installer && git pull)
-
-composer global update
-
-# npm -g upgrade
-
-vagrant plugin update
-
 (cd $DOTFILES && git pull --recurse-submodules)
 
+# Update global composer packages
+composer global update
+
+# Update Laravel Valet
+valet install
+
+# Update vagrant plugins
+vagrant plugin update
+
+# Check for homestead updates
+(cd ~/Homestead && vagrant box update)
+
+# Update volta
+curl https://get.volta.sh | bash
+
+# Upgrade global npm modules?
+# npm -g upgrade
+
+# Change back to the original directory
 cd $cwd

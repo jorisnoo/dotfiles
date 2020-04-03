1ce7c3c7cb2a66d46ef3741231ddfa2e14755dd6
Joris Noordermeer <joris@noordermeer.ch>
2020-04-03 18:16:06 +0200

    Comment unused function

--- zsh/functions.zsh
+++ zsh/functions.zsh
@@ -132,38 +132,38 @@ cleangit () {
 
 
 # Create a .tar.gz archive, using `zopfli`, `pigz` or `gzip` for compression
-targz () {
-    local tmpFile="${@%/}.tar";
-    tar -cvf "${tmpFile}" --exclude=".DS_Store" "${@}" || return 1;
+# targz () {
+#     local tmpFile="${@%/}.tar";
+#     tar -cvf "${tmpFile}" --exclude=".DS_Store" "${@}" || return 1;
 
-    size=$(
-        stat -f"%z" "${tmpFile}" 2> /dev/null; # macOS `stat`
-        stat -c"%s" "${tmpFile}" 2> /dev/null;  # GNU `stat`
-    );
+#     size=$(
+#         stat -f"%z" "${tmpFile}" 2> /dev/null; # macOS `stat`
+#         stat -c"%s" "${tmpFile}" 2> /dev/null;  # GNU `stat`
+#     );
 
-    local cmd="";
-    if (( size < 52428800 )) && hash zopfli 2> /dev/null; then
-        # the .tar file is smaller than 50 MB and Zopfli is available; use it
-        cmd="zopfli";
-    else
-        if hash pigz 2> /dev/null; then
-            cmd="pigz";
-        else
-            cmd="gzip";
-        fi;
-    fi;
+#     local cmd="";
+#     if (( size < 52428800 )) && hash zopfli 2> /dev/null; then
+#         # the .tar file is smaller than 50 MB and Zopfli is available; use it
+#         cmd="zopfli";
+#     else
+#         if hash pigz 2> /dev/null; then
+#             cmd="pigz";
+#         else
+#             cmd="gzip";
+#         fi;
+#     fi;
 
-    echo "Compressing .tar ($((size / 1000)) kB) using \`${cmd}\`…";
-    "${cmd}" -v "${tmpFile}" || return 1;
-    [ -f "${tmpFile}" ] && rm "${tmpFile}";
+#     echo "Compressing .tar ($((size / 1000)) kB) using \`${cmd}\`…";
+#     "${cmd}" -v "${tmpFile}" || return 1;
+#     [ -f "${tmpFile}" ] && rm "${tmpFile}";
 
-    zippedSize=$(
-        stat -f"%z" "${tmpFile}.gz" 2> /dev/null; # macOS `stat`
-        stat -c"%s" "${tmpFile}.gz" 2> /dev/null; # GNU `stat`
-    );
+#     zippedSize=$(
+#         stat -f"%z" "${tmpFile}.gz" 2> /dev/null; # macOS `stat`
+#         stat -c"%s" "${tmpFile}.gz" 2> /dev/null; # GNU `stat`
+#     );
 
-    echo "${tmpFile}.gz ($((zippedSize / 1000)) kB) created successfully.";
-}
+#     echo "${tmpFile}.gz ($((zippedSize / 1000)) kB) created successfully.";
+# }
 
 # Extract most know archives with one command
 extract () {

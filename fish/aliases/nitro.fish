f1c8fc52d064631f7c787c3545fccdffbb371610
Joris Noordermeer <joris@noordermeer.ch>
2021-09-23 11:21:01 +0200

    add nitro

---
+++ fish/aliases/nitro.fish
@@ -0,0 +1,11 @@
+#function homestead
+#    cd ~/Homestead/
+#    vagrant $argv
+#    cd -
+#end
+#
+#alias hu='homestead up'
+#alias he='phpstorm ~/Homestead/Homestead.yaml'
+#alias vm='ssh vagrant@127.0.0.1 -p 2222 -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null'
+
+alias ne='phpstorm ~/.nitro/nitro.yaml'

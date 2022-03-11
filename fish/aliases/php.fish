e6fafc2544a9b86f484078a2a1d25957c5feb4ff
Joris Noordermeer <joris@noordermeer.ch>
2022-03-11 16:02:41 +0200

    update dotfiles

---
+++ fish/aliases/php.fish
@@ -0,0 +1,13 @@
+alias serve='php -S localhost:8000'
+
+alias php74='valet use php@7.4 && composer global update'
+alias php80='valet use php@8.0 && composer global update'
+alias php81='valet use php@8.1 && composer global update'
+
+alias cup='composer update'
+alias cod='composer outdated -D'
+alias ci='composer install'
+alias cda='composer dump-autoload'
+alias cr='composer require'
+
+alias ops='phpstorm .'
--- fish/config.fish
+++ fish/config.fish
@@ -4,13 +4,16 @@ set -x N_PREFIX "$HOME/.local"
 fish_add_path -a $N_PREFIX/bin
 fish_add_path -a $HOME/.local/share/JetBrains/Toolbox/bin
 fish_add_path -a $HOME/.composer/vendor/bin
-fish_add_path -a "./vendor/bin"
+#fish_add_path -a "./vendor/bin"
 fish_add_path -a "$HOME/Library/Preferences/netlify/helper/bin"
 fish_add_path /opt/homebrew/opt/mysql-client/bin
 
-set -gx PROJECT_PATHS ~/Sites/projects ~/Sites/projects/manythingsandlife/
+set -gx PROJECT_PATHS ~/Sites/projects ~/Sites/projects/deinadieu/ ~/Sites/projects/manythingsandlife/
 set -x HOMEBREW_CASK_OPTS "--appdir=/Applications --fontdir=/Library/Fonts"
 
 for aliasfile in ~/.dotfiles/fish/aliases/*.fish
     source $aliasfile
 end
+
+#> echo $fish_user_paths | tr " " "\n" | nl
+#> set --erase --universal fish_user_paths[1]

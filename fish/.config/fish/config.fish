89aaf788599612043f731b74e335151a84687cc6
Joris Noordermeer <joris@noordermeer.ch>
2025-07-01 16:14:25 +0800

    update dotfiles

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,7 @@
-for aliasfile in /Users/jorge/.dotfiles/config/.config/fish/aliases/*.fish
+for aliasfile in ~/.config/fish/aliases/*.fish
     source $aliasfile
 end
 
 # The next line updates PATH for Netlify's Git Credential Helper.
-test -f '/Users/jorge/Library/Preferences/netlify/helper/path.fish.inc' && source '/Users/jorge/Library/Preferences/netlify/helper/path.fish.inc'
+# test -f '~/Library/Preferences/netlify/helper/path.fish.inc' && source '~/Library/Preferences/netlify/helper/path.fish.inc'
 
